3fab55255363b7f5a8c0eeed96def567bb2a3ffc
nafis200 <nafisahamed14@gmail.com>
2024-09-30 16:10:47 +0600

    mangoInformation

--- index.js
+++ index.js
@@ -1,107 +1,135 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { default: axios } = require("axios");
 
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const { default: axios } = require('axios'); 
-app.use(cors({
-   origin: [
-     'http://localhost:5173',
-   ],
-   credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded())
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded());
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8w8siu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-  const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-  };
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
-  async function run() {
-    try { 
-    const userCollection = client.db('MnagoMartDB').collection('accounts')
+async function run() {
+  try {
+    const userCollection = client.db("MnagoMartDB").collection("accounts");
+    const MangoCollection = client.db("MnagoMartDB").collection("mango");
 
-    const verifyToken = async(req,res,next)=>{
-      if(!req.headers.authorization){
-           return res.status(401).send({messsage:'forbidden access'})
+    const verifyToken = async (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ messsage: "forbidden access" });
       }
-      const token = req.headers.authorization.split(' ')[1]
-      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-          if(err){
-             return res.status(401).send({message:'forbidden access'})
-          }
-          req.decoded = decoded
-          next()
-      })
-         
-    }
-  
-// 
-    app.post('/jwt',async(req,res)=>{
-       const user = req.body 
-       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET ,{expiresIn: '1000h'})
-       res
-       .cookie('token',token,cookieOptions)
-       .send({token}) 
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    //
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1000h",
+      });
+      res.cookie("token", token, cookieOptions).send({ token });
       //  .send({success:true})
-    })
+    });
 
-    app.post('/logout',async(req,res)=>{
-        const user = req.body;
-        res.clearCookie('token',{...cookieOptions, maxAge:0}).send({success:true})
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
 
-    })
-
-    app.post('/users',async(req,res)=>{
-      const user = req.body;    
-      const query = {email: user.email}
-      const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message:'user already exists'})
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
       }
-      const cartItem = req.body; 
-      const result = await userCollection.insertOne(cartItem)
-      res.send(result)
- })
-    
+      const cartItem = req.body;
+      const result = await userCollection.insertOne(cartItem);
+      res.send(result);
+    });
 
-  
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      
-    }
+    //  stripe payment gateway
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      console.log("hellow");
+      try {
+        const price = 100;
+        const amount = parseInt(price * 100);
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        res.status(500).send({
+          error: "error from backend",
+        });
+      }
+    });
+
+    //  mango collection
+
+    app.get('/mangoInformation',async(req,res)=>{
+      const result = await MangoCollection.find().toArray();
+      res.send(result);
+    })
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
   }
-  run().catch(console.dir);
-  
-  
-  
-  
-  
-  
-  app.get('/', (req, res) => {
-      res.send('Hello World! it s me how are you i am localhost')
-    })
-  
-  
-  
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
-  
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World! it s me how are you i am localhost");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
